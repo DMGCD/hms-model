2b9aa9006622837ee5025d9efd30e3c37acbbae7
gayan <gcdisanayaka1998@gmail.com>
2024-10-18 21:49:26 +0530

    create Customer Save DTo

---
+++ src/main/java/com/hotel/model_service/dto/customer/CustomerSaveDto.java
@@ -0,0 +1,18 @@
+package com.hotel.model_service.dto.customer;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class CustomerSaveDto {
+    String customerName;
+    String contactNumber;
+    String customerIdNumber;
+    String userName;
+    String password;
+
+
+}
